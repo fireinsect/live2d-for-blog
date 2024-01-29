cc9a2068548e9c4e98c697671bd294b1a2ef4d76
Fireinsect <847954981@qq.com>
2024-01-29 15:40:49 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -71,11 +71,7 @@
             icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">\x3c!--! Font Awesome Free 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. --\x3e<path d="M512 240c0 114.9-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6C73.6 471.1 44.7 480 16 480c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4l0 0 0 0 0 0 0 0 .3-.3c.3-.3 .7-.7 1.3-1.4c1.1-1.2 2.8-3.1 4.9-5.7c4.1-5 9.6-12.4 15.2-21.6c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208z"/></svg>',
             callback: function () {
                 fetch("https://international.v1.hitokoto.cn").then((e => e.json())).then((e => {
-                    if(e.from_who){
-                        const t = `这句一言来自<span>「${e.from_who}」</span>的<span>「${e.from}」</span>。`;
-                    }else{
-                        const t = `这句一言来自<span>「${e.from}」</span>。`;
-                    }
+                    const t = e.from_who ? `这句一言来自<span>「${e.from_who}」</span>的<span>「${e.from}」</span>。` : `这句一言来自<span>「${e.from}」</span>。`;
                     o(e.hitokoto, 6e3, 9), setTimeout((() => {
                         o(t, 4e3, 9)
                     }), 6e3)
